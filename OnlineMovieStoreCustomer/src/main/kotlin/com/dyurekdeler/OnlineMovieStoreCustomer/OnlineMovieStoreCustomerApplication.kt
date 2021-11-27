0d4bb7ce3a6666f4a5d997a63457ec649253bc1e
dyurekdeler <dyurekdeler@live.com>
2021-11-27 12:59:39 +0300

    added OnlineMovieStoreCustomer to microservices

---
+++ OnlineMovieStoreCustomer/src/main/kotlin/com/dyurekdeler/OnlineMovieStoreCustomer/OnlineMovieStoreCustomerApplication.kt
@@ -0,0 +1,11 @@
+package com.dyurekdeler.OnlineMovieStoreCustomer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnlineMovieStoreCustomerApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnlineMovieStoreCustomerApplication>(*args)
+}
---
+++ OnlineMovieStoreCustomer/src/test/kotlin/com/dyurekdeler/OnlineMovieStoreCustomer/OnlineMovieStoreCustomerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dyurekdeler.OnlineMovieStoreCustomer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnlineMovieStoreCustomerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
