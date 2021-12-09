cba760b920cf16aef697b2a908e31e65c1c6fb1a
dyurekdeler <dyurekdeler@live.com>
2021-12-10 01:02:05 +0300

    added config server to eureka as client

--- ConfigServer/build.gradle.kts
+++ ConfigServer/build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	// https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-config-server
 	implementation("org.springframework.cloud:spring-cloud-config-server:3.1.0")
-	// https://mvnrepository.com/artifact/com.netflix.eureka/eureka-client
-	runtimeOnly("com.netflix.eureka:eureka-client:1.10.17")
+// https://mvnrepository.com/artifact/org.springframework.cloud/spring-cloud-starter-netflix-eureka-client
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.1.0")
 
 
 
