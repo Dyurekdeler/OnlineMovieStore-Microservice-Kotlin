67a4ce017f59b75d0a9405a574a86958b26b581f
dyurekdeler <dyurekdeler@live.com>
2021-12-12 18:29:28 +0300

    added mongodb to payment ms

--- OnlineMovieStorePayment/src/main/kotlin/com/dyurekdeler/OnlineMovieStorePayment/entity/Payment.kt
+++ OnlineMovieStorePayment/src/main/kotlin/com/dyurekdeler/OnlineMovieStorePayment/entity/Payment.kt
@@ -3,8 +3,10 @@ package com.dyurekdeler.OnlineMovieStoreCustomer.entity
 import com.dyurekdeler.OnlineMovieStorePayment.model.PaymentMethod
 import org.springframework.data.annotation.Id
 import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
+@Document
 data class Payment (
     @Id
     val id: ObjectId = ObjectId.get(),
